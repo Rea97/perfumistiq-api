db950d71420b70a23f42409ec5720da1ad068f77
Oziel Martínez <omartinez@syscap.com.mx>
2024-09-11 01:44:19 -0600

    style: Correct rubocop offenses (unsafe included)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
-class Users::RegistrationsController < Devise::RegistrationsController
-  include RackSessionsFix
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    include RackSessionsFix
 
-  private
+    private
 
-  def respond_with(current_user, _opts = {})
-    if resource.persisted?
-      render json: {
-        status: { code: 200, message: 'Signed up successfully.' },
-        data: current_user
-      }
-    else
-      render json: {
-        errors: current_user.errors.full_messages
-      }, status: :unprocessable_entity
+    def respond_with(current_user, _opts = {})
+      if resource.persisted?
+        render json: {
+          status: { code: 200, message: 'Signed up successfully.' },
+          data: current_user
+        }
+      else
+        render json: {
+          errors: current_user.errors.full_messages
+        }, status: :unprocessable_entity
+      end
     end
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,29 +1,31 @@
 # frozen_string_literal: true
 
-class Users::SessionsController < Devise::SessionsController
-  include RackSessionsFix
+module Users
+  class SessionsController < Devise::SessionsController
+    include RackSessionsFix
 
-  def respond_with(current_user, _opts = {})
-    render json: {
-      data: {
-        user: current_user,
-        # return token in the body instead of response header but it's not recommended
-        token: request.env['warden-jwt_auth.token']
-      }
-    }, status: :ok
-  end
-
-  def respond_to_on_destroy
-    if current_user
+    def respond_with(current_user, _opts = {})
       render json: {
-        status: 200,
-        message: 'Logged out successfully.'
+        data: {
+          user: current_user,
+          # return token in the body instead of response header but it's not recommended
+          token: request.env['warden-jwt_auth.token']
+        }
       }, status: :ok
-    else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
+    end
+
+    def respond_to_on_destroy
+      if current_user
+        render json: {
+          status: 200,
+          message: 'Logged out successfully.'
+        }, status: :ok
+      else
+        render json: {
+          status: 401,
+          message: "Couldn't find an active session."
+        }, status: :unauthorized
+      end
     end
   end
 end
