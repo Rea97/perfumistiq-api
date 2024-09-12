fee046694dfdf45f56b47c7597d033dcfe71eb07
Oziel Martínez <omartinez@syscap.com.mx>
2024-09-12 00:25:58 -0600

    chore: Add jwt secret config to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Custom configs
+  config.jwt_secret_key = ENV.fetch('DEVISE_JWT_SECRET_KEY')
 end
